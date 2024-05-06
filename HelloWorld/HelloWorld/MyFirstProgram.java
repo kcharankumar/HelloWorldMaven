01826001b6c72bc3b8dad7f94377a9124b11c67c
kcharankumar <47662435+kcharankumar@users.noreply.github.com>
2024-05-06 19:07:29 +0530

    Add files via upload

---
+++ HelloWorld/HelloWorld/MyFirstProgram.java
@@ -0,0 +1,11 @@
+public class MyFirstProgram{
+
+  public static void main (String args[]){
+ 
+    System.out.println ("Hey, Welcome to Jenkins... I am building this program from jenkins..From Charan...");
+
+
+}
+
+
+}
---
+++ HelloWorld/src/test/java/MavenTesting.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MavenTesting {
+	
+	@Test
+	public void test() {
+		boolean bValue = false;
+		assertTrue (bValue);
+	}
+
+}
